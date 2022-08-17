d9f2ddb4098569b9a34f2f72d542296ec46b3741
Fer <mercadofernando.93@gmail.com>
2022-08-17 19:39:34 -0300

    segundo commit de my app

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import './App.css';
+
+function App() {
+ 
+  let estulos = {
+    backgroundColor: "#000",
+    color:"#fff"
+  }
+
+  return (
+    <div id="" className="">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="true" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="navbar-collapse collapse show" id="navbarColor01" style={{}}>
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <a className="nav-link active" href="#">Casa
+            <span className="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Cabecitas</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">A cerca de </a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div className="dropdown-menu">
+            <a className="dropdown-item" href="#">Action</a>
+            <a className="dropdown-item" href="#">Another action</a>
+            <a className="dropdown-item" href="#">Something else here</a>
+            <div className="dropdown-divider" />
+            <a className="dropdown-item" href="#">Separated link</a>
+          </div>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-sm-2" type="text" placeholder="Search" />
+        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+    </div>
+  )
+}
+
+export default App;
+
