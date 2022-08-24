87e46d293ab3ef09536c418edca141b4d042fc82
Fer <mercadofernando.93@gmail.com>
2022-08-24 20:33:08 -0300

    3er desafio

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+import Navbar from './Navbar';
+
+
+const App = () => {
+    return (
+        <div>
+           <Navbar/> 
+        </div>
+    );
+}
+
+export default App;
--- src/components/itemListContainer/itemListContainer.jsx
+++
@@ -1,9 +0,0 @@
-const ItemListContainer = ({saludo}) => {
- 
-    return(
-        <h1>{saludo}</h1>
-    )
-}
-
-
-export default ItemListContainer
